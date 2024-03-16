593bce215db7cfd211ca46985b52fe8bf46ac4ef
dhubmann <daniel.hubmann@gmx.at>
2024-03-16 19:08:05 +0100

    style sidebar

--- practice-4/src/components/Sidebar/index.jsx
+++ practice-4/src/components/Sidebar/index.jsx
@@ -1,15 +1,33 @@
 import "./index.scss";
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faHome,
+  faWrench,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
-      <h3>Navigation</h3>
-      <ul>
-        <li>Link 1</li>
-        <li>Link 2</li>
-        <li>Link 3</li>
-      </ul>
+      <Link className="company-name" to="/">
+        <h3>Company Name</h3>
+      </Link>
+      <nav>
+        <NavLink exact="true" activclassname="active" to="/">
+          <label>Home</label>
+          <FontAwesomeIcon icon={faHome} color="black" />
+        </NavLink>
+        <NavLink exact="true" activclassname="active" to="/edit_items">
+          <label>Edit Items</label>
+          <FontAwesomeIcon icon={faWrench} color="black" />
+        </NavLink>
+        <NavLink exact="true" activclassname="active" to="/">
+          <label>Contact</label>
+          <FontAwesomeIcon icon={faEnvelope} color="black" />
+        </NavLink>
+      </nav>
     </div>
   );
 };
