ce5f158b3200587eab5ed48c17139920c5b2d4ca
dhubmann <daniel.hubmann@gmx.at>
2024-03-16 15:40:43 +0100

    setup platform project

---
+++ practice-4/src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import "./index.scss";
+import React from "react";
+import Sidebar from "../Sidebar";
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ practice-4/src/components/Sidebar/index.jsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <h3>Navigation</h3>
+      <ul>
+        <li>Link 1</li>
+        <li>Link 2</li>
+        <li>Link 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
